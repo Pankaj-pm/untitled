143d86334722a5d9436b8fbe73fb55fd0ab9eb77
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-04-24 09:05:01 +0530

    msg

--- lib/main.dart
+++ lib/main.dart
@@ -6,114 +6,126 @@ import 'package:untitled/util.dart';
 import 'User.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
 
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var themeMode=ThemeMode.system;
+
+  CountController countController = CountController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeMode,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(""),
+          actions: [
+            // Switch(value: isDark, onChanged: (value) {
+            //   isDark=value;
+            //   setState(() {
+            //
+            //   });
+            // },)
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+            IconButton(onPressed: () {
 
-  final String title;
+              print("themeMode $themeMode");
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  CountController countController = CountController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-        itemCount: userList.length,
-        itemBuilder: (context, index) {
-          var user = userList[index];
-          print("user.name ==>${user.name}");
-
-          return ListTile(
-            leading: IconButton(
-              onPressed: () async{
-                await Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return Userdetail(userIndex: index,);
-                  },
-                ));
-                setState(() {
-
-                });
-              },
-              icon: Icon(Icons.remove_red_eye),
-            ),
-            title: TextFormField(
-              controller: TextEditingController(text:user.name ?? "" ),
-              // initialValue: user.name ?? "",
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.remove_circle),
-                ),
-                Text("${user.count ?? 0}"),
-                IconButton(
-                  onPressed: () {
-                    if (user.count == null) {
-                      user.count = 1;
-                    } else {
-                      user.count = user.count! + 1;
-                    }
-                    setState(() {});
-                  },
-                  icon: Icon(Icons.add_box),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-      floatingActionButton: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-
-          FloatingActionButton(
-            onPressed: () async{
-              print("goto detail");
-              await Navigator.push(context, MaterialPageRoute(
-                builder: (context) {
-                  return Userdetail();
-                },
-              ));
-              print("Back to home");
+              if(themeMode==ThemeMode.system){
+                themeMode=ThemeMode.dark;
+              }else if(themeMode==ThemeMode.dark){
+                themeMode=ThemeMode.light;
+              }else{
+                themeMode=ThemeMode.system;
+              }
               setState(() {
 
               });
 
-            },
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        ],
+
+            }, icon: Icon(Icons.settings))
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: userList.length,
+          itemBuilder: (context, index) {
+            var user = userList[index];
+            print("user.name ==>${user.name}");
+
+            return ListTile(
+              leading: IconButton(
+                onPressed: () async {
+                  await Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return Userdetail(
+                        userIndex: index,
+                      );
+                    },
+                  ));
+                  setState(() {});
+                },
+                icon: Icon(Icons.remove_red_eye),
+              ),
+              title: TextFormField(
+                controller: TextEditingController(text: user.name ?? ""),
+                // initialValue: user.name ?? "",
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.remove_circle),
+                  ),
+                  Text("${user.count ?? 0}"),
+                  IconButton(
+                    onPressed: () {
+                      if (user.count == null) {
+                        user.count = 1;
+                      } else {
+                        user.count = user.count! + 1;
+                      }
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.add_box),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        floatingActionButton: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FloatingActionButton(
+              onPressed: () async {
+                print("goto detail");
+                await Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return Userdetail();
+                  },
+                ));
+                print("Back to home");
+                setState(() {});
+              },
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
       ),
     );
   }
